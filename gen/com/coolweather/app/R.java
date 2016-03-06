da915687712217b347935d3cecd34984d29c7ee4
zhaochan <1044359719@qq.com>
2016-03-06 23:48:11 +0800

    完成

--- gen/com/coolweather/app/R.java
+++ gen/com/coolweather/app/R.java
@@ -18,22 +18,40 @@ public final class R {
          would include 7" and 10" devices in landscape (~960dp and ~1280dp respectively).
     
          */
-        public static final int activity_horizontal_margin=0x7f030000;
-        public static final int activity_vertical_margin=0x7f030001;
+        public static final int activity_horizontal_margin=0x7f040000;
+        public static final int activity_vertical_margin=0x7f040001;
     }
     public static final class drawable {
-        public static final int ic_launcher=0x7f020000;
+        public static final int home=0x7f020000;
+        public static final int ic_launcher=0x7f020001;
+        public static final int logo=0x7f020002;
+        public static final int refresh=0x7f020003;
     }
     public static final class id {
-        public static final int action_settings=0x7f070000;
+        public static final int action_settings=0x7f08000b;
+        public static final int city_name=0x7f080003;
+        public static final int current_date=0x7f080007;
+        public static final int list_view=0x7f080001;
+        public static final int publish_text=0x7f080005;
+        public static final int refresh_weather=0x7f080004;
+        public static final int switch_city=0x7f080002;
+        public static final int temp1=0x7f080009;
+        public static final int temp2=0x7f08000a;
+        public static final int title_text=0x7f080000;
+        public static final int weather_desp=0x7f080008;
+        public static final int weather_info_layout=0x7f080006;
+    }
+    public static final class layout {
+        public static final int choose_area=0x7f030000;
+        public static final int weather_layout=0x7f030001;
     }
     public static final class menu {
-        public static final int main=0x7f060000;
+        public static final int main=0x7f070000;
     }
     public static final class string {
-        public static final int action_settings=0x7f040002;
-        public static final int app_name=0x7f040000;
-        public static final int hello_world=0x7f040001;
+        public static final int action_settings=0x7f050002;
+        public static final int app_name=0x7f050000;
+        public static final int hello_world=0x7f050001;
     }
     public static final class style {
         /** 
@@ -57,10 +75,10 @@ public final class R {
     
  API 14 theme customizations can go here. 
          */
-        public static final int AppBaseTheme=0x7f050000;
+        public static final int AppBaseTheme=0x7f060000;
         /**  Application theme. 
  All customizations that are NOT specific to a particular API-level can go here. 
          */
-        public static final int AppTheme=0x7f050001;
+        public static final int AppTheme=0x7f060001;
     }
 }
